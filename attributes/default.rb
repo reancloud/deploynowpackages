0bcd0c7a79acbb669ab3b6fc11d338c0e34d2af4
RahulPatil <rahulpatil18@github.com>
2017-10-06 17:54:01 +0530

    DEp-5127 : Updated path

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,5 +3,5 @@ default['deploynowpackages']['packages'] = []
 if platform?('windows')
   default['deploynowpackages']['packages_home'] = 'C:\\Temp\\cookbooks\\'
 else
-  default['deploynowpackages']['packages_home'] = '/tmp/cookbooks/'
+  default['deploynowpackages']['packages_home'] = '/tmp/reandeploy/cookbooks/'
 end
